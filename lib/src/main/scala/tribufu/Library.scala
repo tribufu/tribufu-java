51263aa8b7da0e882b59a9aeb653ebbf1e04de9e
GuilhermeWerner <26710260+GuilhermeWerner@users.noreply.github.com>
2021-04-17 09:46:47 -0300

    Migrate to Scala Library

---
+++ lib/src/main/scala/tribufu/Library.scala
@@ -0,0 +1,18 @@
+// Copyright (c) TribuFu. All Rights Reserved
+
+package tribufu
+
+class Library {
+    init()
+
+    private def init(){
+        // Used to load the 'TribuFu' library on application startup.
+        //System.loadLibrary("TribuFu")
+    }
+
+    /**
+     * A native method that is implemented by the 'TribuFu' native library, which is packaged with
+     * this library.
+     */
+    //@native def Hello(): Int
+}
---
+++ lib/src/test/scala/tribufu/LibrarySuite.scala
@@ -0,0 +1,16 @@
+// Copyright (c) TribuFu. All Rights Reserved
+
+package tribufu
+
+import org.junit.runner.RunWith
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatestplus.junit.JUnitRunner
+
+@RunWith(classOf[JUnitRunner])
+class LibrarySuite extends AnyFunSuite {
+    test("Hello is always true") {
+        //def library = new Library()
+        //assert(library.Hello())
+        assert(true)
+    }
+}
