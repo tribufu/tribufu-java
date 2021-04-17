c85413b7e06ddb09ffcec4d8af3a7621c6c1dc82
GuilhermeWerner <26710260+GuilhermeWerner@users.noreply.github.com>
2021-04-17 11:46:43 -0300

    Add Sample Classes

---
+++ lib/src/main/scala/tribufu/models/Email.scala
@@ -0,0 +1,10 @@
+// Copyright (c) TribuFu. All Rights Reserved
+
+package tribufu.models
+
+class Email {
+    val address = ""
+    val user_id = "1"
+    val confirmed = false
+    val primary = true
+}
---
+++ lib/src/main/scala/tribufu/models/User.scala
@@ -0,0 +1,53 @@
+// Copyright (c) TribuFu. All Rights Reserved
+
+package tribufu.models
+
+class User {
+    val id = ""
+    val name = ""
+    val display_name = ""
+    val password_hash = ""
+    val role = Role.User
+    val confirmed = false
+    val two_factor_enabled = false
+    val first_name = ""
+    val last_name = ""
+    val verified = false
+    val parental_control = false
+    val profile_url = ""
+    val public_birthday = false
+    val Birthday = ""
+    val location = ""
+    val timezone = ""
+    val language = ""
+    val currency = ""
+    val theme = Theme.Light
+    val ip_address = ""
+    val registration_ip_address = ""
+    val Joined = ""
+    val Updated = ""
+    val failed_logins = 0
+    val about = ""
+    val profile_views = 0
+    val content = 0
+    val avatar_url = ""
+    val background_url = ""
+    val status = Status.Offline
+    val LastOnline = ""
+    val LastActivity = ""
+    val LastPost = ""
+    val warnings = 0
+    val LastWarn = ""
+}
+
+object Role extends Enumeration {
+    val User, Bot, Helper, Tester, Developer, Moderator, Admin, Root = Value
+}
+
+object Theme extends Enumeration {
+    val Light, Dark = Value
+}
+
+object Status extends Enumeration {
+    val Offline, Away, Disturb, Online, Playing = Value
+}
