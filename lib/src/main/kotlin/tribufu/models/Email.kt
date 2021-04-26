e5858aeb537911c13e61f09ac155a3a75ad1d291
GuilhermeWerner <26710260+GuilhermeWerner@users.noreply.github.com>
2021-04-26 09:42:59 -0300

    Add Sample Models

---
+++ lib/src/main/kotlin/tribufu/models/Email.kt
@@ -0,0 +1,10 @@
+// Copyright (c) TribuFu. All Rights Reserved
+
+package tribufu.models
+
+class Email {
+    var address = ""
+    var user_id = "1"
+    var confirmed = false
+    var primary = true
+}
