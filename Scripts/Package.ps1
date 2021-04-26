ccc99b540145a51cd78fa73fa9b3aba6146313d5
GuilhermeWerner <26710260+GuilhermeWerner@users.noreply.github.com>
2021-04-26 20:26:49 -0300

    Update Package.ps1

--- Scripts/Package.ps1
+++ Scripts/Package.ps1
@@ -5,4 +5,4 @@
 
 New-Item -Path "./Release" -ItemType Directory -Force
 
-Copy-Item -Path "./lib/build/libs/lib-0.0.1.jar" -Destination "./Release/TribuFu-0.0.1.jar" -Recurse -Force
+Copy-Item -Path "./lib/build/libs/lib-0.0.1.jar" -Destination "./Release/TribuFu.jar" -Recurse -Force
