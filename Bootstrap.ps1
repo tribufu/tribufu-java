373031534eca915af24e7f0662a8e5cc79533104
GuilhermeWerner <26710260+GuilhermeWerner@users.noreply.github.com>
2021-05-05 10:28:44 -0300

    Update Bootstrap Scripts

--- Bootstrap.ps1
+++ Bootstrap.ps1
@@ -1,10 +1,36 @@
 #!/usr/bin/env pwsh
 
+# Windows
+
 if ($IsWindows)
 {
+    echo "Generating Visual Studio project..."
+
+    & "./Vendor/Premake/Windows/premake5.exe" "vs2019"
 }
-else
+
+# Mac
+
+elseif ($IsMacOS)
 {
     sudo chmod +x ./gradlew
     sudo chmod +x ./Scripts/Package.ps1
+
+    echo "Generating XCode project..."
+
+    sudo chmod +x ./Vendor/Premake/Mac/premake5
+    & "./Vendor/Premake/Mac/premake5" "xcode4"
+}
+
+# Linux
+
+elseif ($IsLinux)
+{
+    sudo chmod +x ./gradlew
+    sudo chmod +x ./Scripts/Package.ps1
+
+    echo "Generating GMake project..."
+
+    sudo chmod +x ./Vendor/Premake/Linux/premake5
+    & "./Vendor/Premake/Linux/premake5" "gmake2"
 }
--- Bootstrap.sh
+++ Bootstrap.sh
@@ -2,3 +2,22 @@
 
 sudo chmod +x ./gradlew
 sudo chmod +x ./Scripts/Package.ps1
+
+# Linux
+
+if [ "$(expr substr $(uname -s) 1 5)" = "Linux" ]
+then
+    echo "Generating GMake project..."
+
+    sudo chmod +x ./Vendor/Premake/Linux/premake5
+    ./Vendor/Premake/Linux/premake5 gmake2
+
+# Mac
+
+elif [ "$(uname)" = "Darwin" ]
+then
+    echo "Generating XCode project..."
+
+    sudo chmod +x ./Vendor/Premake/Mac/premake5
+    ./Vendor/Premake/Mac/premake5 xcode4
+fi
