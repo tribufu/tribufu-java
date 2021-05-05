0b1f87d079703e098ca67833579d776ed423778c
Guilherme <guilhermeqwerner@gmail.com>
2021-05-05 10:34:49 -0300

    Update Import/Export Macros

--- Native/Source/Library.h
+++ Native/Source/Library.h
@@ -1,7 +1,19 @@
 // Copyright (c) TribuFu. All Rights Reserved
 
+#pragma once
+
 #include "TribuFu.h"
 
-#define EXPORT __declspec(dllexport)
+#if defined(_MSC_VER)
+    #define EXPORT __declspec(dllexport)
+    #define IMPORT __declspec(dllimport)
+#elif defined(__GNUC__)
+    #define EXPORT __attribute__((visibility("default")))
+    #define IMPORT
+#else
+    #define EXPORT
+    #define IMPORT
+    #pragma warning Unknown dynamic link import/export semantics.
+#endif
 
 EXPORT char *Java_Hello(char *input);
