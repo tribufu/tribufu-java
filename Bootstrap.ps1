30e376c1e8be977258c63393cb59228741cc523c
GuilhermeWerner <26710260+GuilhermeWerner@users.noreply.github.com>
2021-05-07 09:38:57 -0300

    Revert "Update Bootstrap Scripts"
    This reverts commit 717d3d00efbb677c54c4323d678725a0c515838f.

--- Bootstrap.ps1
+++ Bootstrap.ps1
@@ -4,6 +4,9 @@
 
 if ($IsWindows)
 {
+    echo "Generating Visual Studio project..."
+
+    & "./Vendor/Premake/Windows/premake5.exe" "vs2019"
 }
 
 # Mac
@@ -12,6 +15,11 @@ elseif ($IsMacOS)
 {
     sudo chmod +x ./gradlew
     sudo chmod +x ./Scripts/Package.ps1
+
+    echo "Generating XCode project..."
+
+    sudo chmod +x ./Vendor/Premake/Mac/premake5
+    & "./Vendor/Premake/Mac/premake5" "xcode4"
 }
 
 # Linux
@@ -20,4 +28,9 @@ elseif ($IsLinux)
 {
     sudo chmod +x ./gradlew
     sudo chmod +x ./Scripts/Package.ps1
+
+    echo "Generating GMake project..."
+
+    sudo chmod +x ./Vendor/Premake/Linux/premake5
+    & "./Vendor/Premake/Linux/premake5" "gmake2"
 }
--- Bootstrap.sh
+++ Bootstrap.sh
@@ -7,10 +7,17 @@ sudo chmod +x ./Scripts/Package.ps1
 
 if [ "$(expr substr $(uname -s) 1 5)" = "Linux" ]
 then
+    echo "Generating GMake project..."
+
+    sudo chmod +x ./Vendor/Premake/Linux/premake5
+    ./Vendor/Premake/Linux/premake5 gmake2
 
 # Mac
 
 elif [ "$(uname)" = "Darwin" ]
 then
+    echo "Generating XCode project..."
 
+    sudo chmod +x ./Vendor/Premake/Mac/premake5
+    ./Vendor/Premake/Mac/premake5 xcode4
 fi
