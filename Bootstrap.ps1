717d3d00efbb677c54c4323d678725a0c515838f
GuilhermeWerner <26710260+GuilhermeWerner@users.noreply.github.com>
2021-05-07 08:44:11 -0300

    Update Bootstrap Scripts

--- Bootstrap.ps1
+++ Bootstrap.ps1
@@ -4,9 +4,6 @@
 
 if ($IsWindows)
 {
-    echo "Generating Visual Studio project..."
-
-    & "./Vendor/Premake/Windows/premake5.exe" "vs2019"
 }
 
 # Mac
@@ -15,11 +12,6 @@ elseif ($IsMacOS)
 {
     sudo chmod +x ./gradlew
     sudo chmod +x ./Scripts/Package.ps1
-
-    echo "Generating XCode project..."
-
-    sudo chmod +x ./Vendor/Premake/Mac/premake5
-    & "./Vendor/Premake/Mac/premake5" "xcode4"
 }
 
 # Linux
@@ -28,9 +20,4 @@ elseif ($IsLinux)
 {
     sudo chmod +x ./gradlew
     sudo chmod +x ./Scripts/Package.ps1
-
-    echo "Generating GMake project..."
-
-    sudo chmod +x ./Vendor/Premake/Linux/premake5
-    & "./Vendor/Premake/Linux/premake5" "gmake2"
 }
