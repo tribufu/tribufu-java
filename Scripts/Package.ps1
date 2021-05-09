7583e38f38605d7fa4042236c80c5145b0256822
GuilhermeWerner <26710260+GuilhermeWerner@users.noreply.github.com>
2021-05-09 17:03:08 -0300

    Make JNI Bindings in Rust

--- Scripts/Package.ps1
+++ Scripts/Package.ps1
@@ -4,5 +4,11 @@
 ./gradlew jar
 
 New-Item -Path "./Release" -ItemType Directory -Force
+New-Item -Path "./Release/Windows" -ItemType Directory -Force
+New-Item -Path "./Release/Mac" -ItemType Directory -Force
+New-Item -Path "./Release/Linux" -ItemType Directory -Force
+New-Item -Path "./Release/Android" -ItemType Directory -Force
 
 Copy-Item -Path "./lib/build/libs/lib-0.0.1.jar" -Destination "./Release/TribuFu.jar" -Recurse -Force
+
+Copy-Item -Path "./Binaries/debug/TribuFu_jvm.dll" -Destination "./Release/Windows/TribuFu_jvm.dll" -Recurse -Force
---
+++ Source/lib.rs
@@ -0,0 +1,41 @@
+// Copyright (c) TribuFu. All Rights Reserved
+
+//! Jvm bindings to TribuFu SDK.
+
+#![allow(non_snake_case)]
+#![allow(unused_variables)]
+#![allow(unused_imports)]
+
+#[cfg(not(any(
+    target_os = "windows",
+    target_os = "macos",
+    target_os = "linux",
+    target_os = "android",
+)))]
+compile_error!("INVALID_TARGET_PLATFORM");
+
+use jni::objects::{JClass, JString};
+use jni::sys::jstring;
+use jni::JNIEnv;
+use std::ffi::{CStr, CString};
+
+#[no_mangle]
+// This is the class that owns our static method. It's not going to be used,
+// but still must be present to match the expected signature of a static
+// native method.
+pub extern "system" fn Java_tribufu_TribuFu_Hello(
+    env: JNIEnv,
+    _: JClass,
+    j_input: JString,
+) -> jstring {
+    // First, we have to get the string out of Java. Check out the `strings`
+    // module for more info on how this works.
+    let input = CString::from(unsafe { CStr::from_ptr(env.get_string(j_input).unwrap().as_ptr()) });
+
+    // Then we have to create a new Java string to return. Again, more info
+    // in the `strings` module.
+    let res = env.new_string(input.to_str().unwrap()).unwrap();
+
+    // Finally, extract the raw pointer to return.
+    return res.into_inner();
+}
