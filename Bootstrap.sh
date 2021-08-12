2f07cf5c4c6e41f66204996d807039408853746c
GuilhermeWerner <26710260+GuilhermeWerner@users.noreply.github.com>
2021-08-12 08:36:09 -0300

    Update scripts

--- Bootstrap.sh
+++ Bootstrap.sh
@@ -1,16 +1,1 @@
 #!/usr/bin/env sh
-
-sudo chmod +x ./gradlew
-sudo chmod +x ./Scripts/Package.ps1
-
-# Linux
-
-if [ "$(expr substr $(uname -s) 1 5)" = "Linux" ]
-then
-
-# Mac
-
-elif [ "$(uname)" = "Darwin" ]
-then
-
-fi
