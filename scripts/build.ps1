475d1c5dc62fa5a77282a58ad74c727209b9aac3
Guilherme Werner <guilherme.werner@tribufu.com>
2024-02-18 19:55:26 -0300

    Use mintaka from maven repo

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -1,8 +1,3 @@
 #!/usr/bin/env pwsh
 
 .\gradlew build
-
-New-Item "build/libs" -ItemType Directory -Force
-Remove-Item -Path "build/libs/*" -Force
-Copy-Item -Path "./lib/build/libs/*.jar" -Destination "./build/libs" -Force
-Copy-Item -Path "./vendor/MintakaJvm/lib/build/libs/*.jar" -Destination "./build/libs" -Force
